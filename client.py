0c18217137425ae4083f40ea5e70898992003c60
Helush <diefzbr@gmail.com>
2025-12-04 13:18:14 +0200

    cashier panel(client)

--- client.py
+++ client.py
@@ -1,16 +1,171 @@
-import socket
+from socket import *
+from tkinter import *
+from tkinter import messagebox
+from datetime import datetime
 
 HOST = "127.0.0.1"
 PORT = 5000
 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client = socket(AF_INET, SOCK_STREAM)
 client.connect((HOST, PORT))
 
-while True:
-    in_data = client.recv(1024).decode()
-    print("Message from the server ", in_data)
-    out_data = input("Enter your question: ")
-    client.send(out_data.encode())
-    if out_data == "bye":
-        break
+
+class LoginScreen(Frame):
+    def __init__(self, client):
+        Frame.__init__(self)
+        self.pack()
+        self.client = client
+        self.master.title("Login")
+
+        self.frame1 = Frame(self)
+        self.frame1.pack(padx = 5, pady=5)
+
+        self.userNameLabel = Label(self.frame1, text="Username: ")
+        self.userNameLabel.pack(padx=5, pady=5, side=LEFT)
+
+        self.userNameEntry = Entry(self.frame1)
+        self.userNameEntry.pack(padx=5, pady=5, side=LEFT)
+
+        self.frame2 = Frame(self)
+        self.frame2.pack(padx= 5, pady=5)
+
+        self.passwordLabel = Label(self.frame2, text="Password: ")
+        self.passwordLabel.pack(padx=5, pady=5, side=LEFT)
+
+        self.passwordEntry = Entry(self.frame2, show="*")
+        self.passwordEntry.pack(padx=5, pady=5, side=LEFT)
+
+        self.frame3 = Frame(self)
+        self.frame3.pack(padx=5, pady= 5)
+
+        self.loginButton = Button(self.frame3, text="Login", command = self.ButtonPressed)
+        self.loginButton.pack(padx=5, pady=5, side=LEFT)
+
+    def ButtonPressed(self):
+        out_data = f"login;{self.userNameEntry.get()};{self.passwordEntry.get()}"
+        self.client.send(out_data.encode())
+
+        response = self.client.recv(1024).decode().split(";")
+
+        if response[0] == "loginsuccess":
+            print("Login successfulllll")
+            username = response[1]
+            role = response[2]
+
+            self.master.destroy()
+
+            if role == "Cashier":
+                CashierPanel(self.client, username)
+            elif role == "Manager":
+                ManagerPanel(self.client, username)
+            else:
+                print("Invalid role")
+        elif response[0] == "loginfailure":
+            messagebox.showerror("Login Failed", "Invalid username or password!")
+
+
+
+class CashierPanel(Frame):
+    def __init__(self, cashier, username):
+        Frame.__init__(self)
+        self.cashier = cashier
+        self.username = username
+        self.bookList = [] # keep track of added books
+        self.pack()
+        self.master.title("Transaction")
+
+        self.frame1 = Frame(self)
+        self.frame1.pack(padx = 5, pady=5)
+
+        self.bookIDLabel = Label(self.frame1, text="Book ID: ")
+        self.bookIDLabel.pack(padx=5, pady=5, side=LEFT)
+
+        self.bookIDEntry = Entry(self.frame1)
+        self.bookIDEntry.pack(padx=5, pady=5, side=LEFT)
+
+        self.frame2 = Frame(self)
+        self.frame2.pack(padx= 5, pady=5)
+
+        self.quantityLabel = Label(self.frame2, text="Quantity: ")
+        self.quantityLabel.pack(padx=5, pady=5, side=LEFT)
+
+        self.quantityEntry = Entry(self.frame2)
+        self.quantityEntry.pack(padx=5, pady=5, side=LEFT)
+
+        self.frame3 = Frame(self)
+        self.frame3.pack(padx=5, pady= 5)
+
+        self.addButton = Button(self.frame3, text="Add Book", command = self.addButtonPressed)
+        self.addButton.pack(padx=5, pady=5, side=LEFT)
+
+        self.frame3 = Frame(self) #  add book
+        self.frame3.pack(padx = 5, pady = 5)
+
+        self.addLabel = Label(self.frame3, text="Books Added:")
+        self.addLabel.pack(padx = 5, pady = 5, side = LEFT)
+
+        self.frame4 = Frame(self)
+        self.frame4.pack(padx = 5, pady = 5)
+
+        self.bookEntry = Text(self.frame4, width = 25, height = 5)
+        self.bookEntry.pack(padx = 5, pady = 5, side = LEFT)
+
+        self.frame5 = Frame(self) # discount entry
+        self.frame5.pack(padx = 5, pady = 5)
+
+        self.discountLabel = Label(self.frame5, text="Discount Code: ")
+        self.discountLabel.pack(padx = 5, pady = 5, side = LEFT)
+
+        self.discountEntry = Entry(self.frame5)
+        self.discountEntry.pack(padx = 5, pady= 5, side = LEFT)
+
+        self.frame6 = Frame(self) # close button
+        self.frame6.pack(padx = 5, pady = 5)
+
+        self.closeButton = Button(self.frame6, text = "Close", command = self.closeButtonPressed)
+        self.closeButton.pack (padx = 5, pady = 5, side =LEFT)
+
+        self.transactionButton = Button(self.frame6, text="Complete Transaction", command = self.transactionButtonPressed)
+        self.transactionButton.pack(padx = 5, pady = 5, side = LEFT)
+
+
+    def closeButtonPressed(self):
+        print("close button pressed")
+
+    def addButtonPressed(self):
+        print("add Button pressed")
+        book_id = self.bookIDEntry.get()
+        quantity = self.quantityEntry.get()
+        book = f"{book_id}-{quantity}"
+        self.bookList.append(book)
+        self.bookEntry.insert(END, book + "\n")
+
+    def transactionButtonPressed(self):
+        print("Transaction button pressed")
+        date = datetime.now()
+        discount_code = self.discountEntry.get()
+        books = ";".join(self.bookList)
+        data_out = f"transaction;{date};{discount_code};{books}"
+
+        self.cashier.send(data_out.encode())
+
+        #if response[0] == "transactionconfirmation": h
+        #elif response[0] == "transactionfailure":
+
+
+
+
+class ManagerPanel(Frame):
+    def __init__(self, client, username):
+        Frame.__init__(self)
+        self.client = client
+        self.username = username
+
+
+in_data = client.recv(1024).decode()
+print("Message from the server:", in_data)
+
+window = LoginScreen(client)
+window.mainloop()
+
 client.close()
--- server.py
+++ server.py
@@ -1,35 +1,145 @@
-import socket
-import threading
+from socket import *
+from threading import *
 
-class ClientThread(threading.Thread):
-    def __init__(self, clientSocket, clientAddress):
-        threading.Thread.__init__(self)
-        self.clientSocket =  clientSocket
+
+class ClientThread(Thread):
+    def __init__(self, clientSocket, clientAddress, user_records):
+        Thread.__init__(self)
+        self.clientSocket = clientSocket
         self.clientAddress = clientAddress
-        print("New connection added from ", clientAddress)
+        self.user_records = user_records
+        self.discount_records = discount_records
+        print("New connection added from", clientAddress)
 
     def run(self):
-        msg = "welcome".encode()
+        msg = "connectionsuccess".encode()
         self.clientSocket.send(msg)
-        while True:
-            data = self.clientSocket.recv(1024).decode()
-            if data == "bye":
-                break
-            msg = str(eval(data)).encode()
-            self.clientSocket.send(msg)
-        self.clientSocket.close()
 
+        while True:
+            try:
+                data = self.clientSocket.recv(1024).decode()
+                data = data.split(";")
+
+                if data[0] == "login":
+
+                    username = data[1]
+                    password = data[2]
+
+                    if username == "" or password == "":
+                        msg = f"loginfailure".encode()
+                        self.clientSocket.send(msg)
+                        self.clientSocket.close()
+                        return
+
+
+                    found = False
+                    user_role = ""
+                    for record in self.user_records:
+                        record = record.strip()
+
+                        record = record.split(";")
+                        stored_username = record[0]
+                        stored_password = record[1]
+
+                        if username == stored_username and password == stored_password:
+                            found = True
+                            user_role = record[2]
+                            break
+                    if found:
+                        msg = f"loginsuccess;{username};{user_role}".encode()
+                    else:
+                        msg = f"loginfailure".encode()
+
+                    self.clientSocket.send(msg)
+
+                elif data[0] == "transaction":
+                    date = data[1]
+                    discount_code = data[2]
+                    books = data[3]
+
+                    if(books == ""): # if no books added
+                        msg = f"transactionfailure;bookNotAdded".encode()
+                        self.clientSocket.send(msg)
+                        self.clientSocket.close()
+                        return
+
+                    found = False
+                    for record in self.discount_records:
+                        record = record.split()
+                        if(record == discount_code):
+                            found = True
+                            break
+                        if not found:
+                            msg = f"transactionfailure;incorrectDiscountCode".encode()
+
+
+                    self.clientSocket.send(msg)
+
+
+
+
+            except Exception:
+                print(f"Error handling client {self.clientAddress}")
+            finally:
+                self.clientSocket.close()
+                print(f"Connection closed from {self.clientAddress}")
+
+def calculateTotalPrice(books):
+    totalPrice = 0
+    book = books.split(";")
+    for item in book:
+        bookID = item.split("-")[0]
+        quantity = item.split("-")[1]
+        quantity = int(quantity)
+        try:
+            file = open("inventory.txt", "r")
+            inventory_records = file.readlines()
+            if inventory_records.split(";")[0] == bookID:
+                price = float(inventory_records.split(";")[4])
+                totalPrice += price * quantity
+
+        except FileNotFoundError:
+            print("inventory.txt not found")
+            exit(1)
+
+    return totalPrice
+
+
+
+
+
+# Start TCP on 127.0.0.1 5000
 HOST = "127.0.0.1"
 PORT = 5000
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+server = socket(AF_INET, SOCK_STREAM)
+server.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
 server.bind((HOST, PORT))
 print("Server started!")
 print("Waiting for connection requests")
 
+try:
+    file =  open("users.txt", "r")
+    user_records = file.readlines()
+    file.close()
+except FileNotFoundError:
+    print("users.txt file not found")
+    exit(1)
+
+try:
+    discountFile = open("discounts.txt", "r")
+    discount_records = discountFile.readlines()
+    file.close()
+
+except FileNotFoundError:
+    print("discounts.txt file not found")
+    exit(1)
+
+
+
 while True:
     server.listen()
     clientSocket, clientAddress = server.accept()
-    newThread = ClientThread(clientSocket, clientAddress)
+
+    newThread = ClientThread(clientSocket, clientAddress, user_records[:]) #passes a copy to each thread..
     newThread.start()
